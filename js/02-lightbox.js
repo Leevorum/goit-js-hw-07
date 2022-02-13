9a78de01115ee65cafe70e7f2d3cb3dfa029a5bb
Leevorum <di4111ua@gmail.com>
2022-02-13 17:15:31 +0200

    task 2 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 console.log(galleryItems);
+// 1)Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+const galleryDivElem = document.querySelector(".gallery");
+console.log(galleryDivElem);
+// делаем рендер галереии
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+                <a class="gallery__item" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        alt="${description}"
+                    />
+                </a>`;
+    })
+    .join("");
+}
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryDivElem.insertAdjacentHTML("beforeend", galleryMarkup);
+// Добавляем модалку с подписями из библиотеки
+new SimpleLightbox(".gallery a", {
+  // Задержка появления подписи
+  captionDelay: 250,
+  captionSelector: "img",
+  //   Берем подпись из альта картинки
+  captionsData: "alt",
+  //   Ставим подпись вниз картинки
+  captionPosition: "bottom"
+});
